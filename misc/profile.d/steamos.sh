1444d1fb8c2d62af7cdd73a50068c1cd809d203e
Guilherme G. Piccoli <gpiccoli@igalia.com>
2022-12-05 18:30:32 -0300

    misc: Reduce dracut output pollution
    After the movement to btrfs as the root filesystem, due to some
    incompatibility with extended attributes, dracut dumps >200K of
    harmless yet alarming messages during the initramfs creation.
    
    Not that a regular user creates initrams in a daily basis, but
    this is quite annoying for developers or any user installing
    their own kernels. Let's get rid of that using this dracut
    tuning.
    
    References for some analysis about that:
    
    https://gitlab.steamos.cloud/holo/holo/-/merge_requests/107#note_46441
    https://gitlab.steamos.cloud/jupiter/tasks/-/issues/640#note_73458
    
    Signed-off-by: Guilherme G. Piccoli <gpiccoli@igalia.com>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- misc/profile.d/steamos.sh
+++ misc/profile.d/steamos.sh
@@ -9,3 +9,8 @@ then
 	VISUAL=gvim
 	export VISUAL
 fi
+
+# Don't copy xattr(s), since it will fail due to our FS setup.
+# The rootfs being brtfs and the /etc overlay ext4.
+# https://gitlab.steamos.cloud/holo/holo/-/merge_requests/107#note_46441
+export DRACUT_NO_XATTR=1
